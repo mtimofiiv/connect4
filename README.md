f7250f6ed1dc036b6a068ab1d039c98cd2d4c643
Mike Timofiiv <mike.timofiiv@gmail.com>
2015-02-03 22:19:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Connect 4
+
+The classic game, created in a neat little web package for the hell of it.
---
+++ assets/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function($, _) {
+
+  var App = {};
+
+  App.initialize = function() {
+    App.container = $('#application');
+  };
+
+  App.initialize();
+
+})($, _);
